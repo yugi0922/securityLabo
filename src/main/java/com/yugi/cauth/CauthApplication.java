c71930c936d1aea66c5c98cd95fc0a92f8a82070
dxsui-ysuwa <suwa.yuji@nci.co.jp>
2024-01-10 08:59:40 +0900

    initial commit

---
+++ src/main/java/com/yugi/cauth/CauthApplication.java
@@ -0,0 +1,13 @@
+package com.yugi.cauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yugi/cauth/CauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yugi.cauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
